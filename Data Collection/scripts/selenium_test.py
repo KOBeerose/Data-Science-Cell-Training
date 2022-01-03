5403967d10b70221d60f7af108507abd4ebe2d49
KOBeerose <tahaelghabi@gmail.com>
2022-01-03 21:20:51 +0100

    getting info from file

--- Data Collection/scripts/selenium_test.py
+++ Data Collection/scripts/selenium_test.py
@@ -1,21 +1,42 @@
+#importing all the necessary libaries
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import string
 
+url = email = password = ''
+# defining the webdriver and config btw this code will be almost the same in all of your selenium scripts
 chrome_options = webdriver.ChromeOptions()
 
-prefs = {"profile.default_content_setting_values.notifications" : 2}
+# !!! blocking browser notifications !!!
+prefs = {"profile.default_content_setting_values.notifications" : 2} 
 chrome_options.add_experimental_option("prefs", prefs)
+
+# starting in maximized window
 chrome_options.add_argument("start-maximized")
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="C:\\Users\\taha_\\Downloads\\chromedriver_win32\\chromedriver.exe")
 
-driver.get("https://www.facebook.com/")
+
+# this function gets the user info and save them in variables
+def get_info(filename): 
+    elem_list=["url","email","password"]   # list of expressions preceding the user info
+    with open(filename, 'r') as read_obj: 
+            for line in read_obj:
+                for elem in elem_list:  # Get the value after an expression occurrence
+                    if elem in line:
+                        # save the value in a variable named according to the expression preceding it
+                        globals()[elem] = line.partition(elem+": ")[2].translate({ord(c): None for c in string.whitespace})   
+
+# getting values of the user information
+get_info("C:\\Users\\taha_\\OneDrive - ine.inpt.ma\\Coding\\Data Science\\Data-Science-Cell-Training\\Data Collection\\scripts\\Information.txt")
+print(url, email, password)
+driver.get(url)
 
 mail = driver.find_element_by_name("email")
-mail.send_keys("tahaelghabi@gmail.com")
+mail.send_keys(email)
 
 passwrd = driver.find_element_by_name("pass")
-passwrd.send_keys("emmm")
+passwrd.send_keys(password)
 
 
 log = driver.find_element_by_name("login")
